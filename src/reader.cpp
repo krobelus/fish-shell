d23ad2b517c7697b9635cb318474dc4661d0c7bb
ridiculousfish <corydoras@ridiculousfish.com>
2020-03-03 00:13:33 -0800

    Adopt debounce for highlighting and autosuggestions
    This prevents a thundering herd of threads for certain interactive
    scenarios.
    
    cherry-picked from e334bec

--- src/reader.cpp
+++ src/reader.cpp
@@ -144,6 +144,20 @@ operation_context_t get_bg_context(const std::shared_ptr<environment_t> &env,
     return operation_context_t{nullptr, *env, std::move(cancel_checker)};
 }
 
+/// Get the debouncer for autosuggestions and background highlighting.
+/// These are deliberately leaked to avoid shutdown dtor registration.
+static debounce_t &debounce_autosuggestions() {
+    const long kAutosuggetTimeoutMs = 500;
+    static debounce_t *res = new debounce_t(kAutosuggetTimeoutMs);
+    return *res;
+}
+
+static debounce_t &debounce_highlighting() {
+    const long kHighlightTimeoutMs = 500;
+    static debounce_t *res = new debounce_t(kHighlightTimeoutMs);
+    return *res;
+}
+
 void editable_line_t::insert_string(const wcstring &str, size_t start, size_t len) {
     // Clamp the range to something valid.
     size_t string_length = str.size();
@@ -1373,9 +1387,10 @@ void reader_data_t::update_autosuggestion() {
         const editable_line_t *el = active_edit_line();
         auto performer = get_autosuggestion_performer(parser(), el->text, el->position, history);
         auto shared_this = this->shared_from_this();
-        iothread_perform(performer, [shared_this](autosuggestion_result_t result) {
-            shared_this->autosuggest_completed(std::move(result));
-        });
+        debounce_autosuggestions().perform(
+            performer, [shared_this](autosuggestion_result_t result) {
+                shared_this->autosuggest_completed(std::move(result));
+            });
     }
 }
 
@@ -2075,9 +2090,10 @@ void reader_data_t::super_highlight_me_plenty(int match_highlight_pos_adjust, bo
     } else {
         // Highlighting including I/O proceeds in the background.
         auto shared_this = this->shared_from_this();
-        iothread_perform(highlight_performer, [shared_this](highlight_result_t result) {
-            shared_this->highlight_complete(std::move(result));
-        });
+        debounce_highlighting().perform(highlight_performer,
+                                        [shared_this](highlight_result_t result) {
+                                            shared_this->highlight_complete(std::move(result));
+                                        });
     }
     highlight_search();
 
